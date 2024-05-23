188e11cf4bd0fba48001742bb0fc0f033afa04c3
12Ahn22 <12ayo22@gmail.com>
2024-05-23 11:54:18 +0900

    feat: Footer 컴포넌트 작업 - 팀원 소개, 레포지토리 클릭 시 새 창으로 링크 이동 - 화면 작업 하드코딩 상태임, 후에 변경할 요소가 있는 경우 그때 작업

---
+++ src/components/common/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <footer class="w-full bg-white py-10 border-t border-solid">
+    <div class="container mx-auto flex flex-row flex-nowrap justify-between">
+      <div class="flex flex-row flex-wrap">
+        <div class="mr-8">
+          <h2 class="font-bold mb-2">이용안내</h2>
+          <ul class="space-y-1">
+            <li class="text-sm text-gray-600">접수 기준</li>
+            <li class="text-sm text-gray-600">이용 정책</li>
+            <li class="text-sm text-gray-600">커뮤니티 가이드라인</li>
+          </ul>
+        </div>
+        <div>
+          <h2 class="font-bold mb-2">팀원 소개</h2>
+          <ul class="space-y-1">
+            <li class="text-sm text-gray-600 hover:text-blue-500"><a href="https://github.com/12Ahn22" target="_blank">안아영</a></li>
+            <li class="text-sm text-gray-600 hover:text-blue-500"><a href="https://github.com/bizbaeja" target="_blank">배자현</a></li>
+            <li class="text-sm text-gray-600 hover:text-blue-500"><a href="https://github.com/imhungry2" target="_blank">박상준</a></li>
+            <li class="text-sm text-gray-600 hover:text-blue-500"><a href="https://github.com/kgpwls7357" target="_blank">김혜진</a></li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <h2 class="font-bold mb-2">깃허브 레포지토리</h2>
+        <ul>
+          <li><a href="https://github.com/GNDVMall" class="text-gray-600 hover:text-blue-500 text-sm">Organization</a></li>
+          <li><a href="https://github.com/GNDVMall/GNDV-back" class="text-gray-600 hover:text-blue-500 text-sm">Backend Repository</a></li>
+          <li><a href="https://github.com/GNDVMall/GNDV-front" class="text-gray-600 hover:text-blue-500 text-sm">Frontend Repository</a></li>
+          <li><a href="https://github.com/GNDVMall/GNDV-admin" class="text-gray-600 hover:text-blue-500 text-sm">Admin Repository</a></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
--- src/components/common/Layout.vue
+++ src/components/common/Layout.vue
@@ -1,11 +1,13 @@
 <script setup>
   import Header from './Header.vue'
+  import Footer from './Footer.vue'
 </script>
 
 <template>
   <div id="layout" class="container mx-auto">
     <Header />
     <slot />
+    <Footer />
   </div>
 </template>
 
