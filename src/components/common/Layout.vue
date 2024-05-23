1fb7d78c6d0780e831c84026f0be2042d72182fc
12Ahn22 <12ayo22@gmail.com>
2024-05-23 10:16:23 +0900

    feat: layout 컴포넌트 생성

---
+++ src/components/common/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="layout" class="container mx-auto">
+    헤더
+    <slot />
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
