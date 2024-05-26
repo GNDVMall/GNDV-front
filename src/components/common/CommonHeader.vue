b3c5f68a4714a49a70888c8b2f9894aa4425c1bd
bizbaeja <bjgbjg112@gmail.com>
2024-05-26 13:44:24 +0900

    feat: CommonHeader 추가

---
+++ src/components/common/CommonHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <header class=" py-4">
+      <div class="container mx-auto">
+        <div class="flex items-center">
+          <h1 class="text-xl font-bold">로그인 정보</h1>
+        </div>
+        <div class="relative mt-2">
+          <div class="absolute inset-x-0 top-[-8px] h-1 "></div>
+          <div class="h-1 bg-black mt-2"></div>
+        </div>
+      </div>
+    </header>
+  </template>
+  
+  <script setup>
+  // Here you can add any necessary script, if needed.
+  </script>
+  
+  <style scoped>
+  /* Custom styles can be added here if needed. */
+  </style>
+  
--- src/components/common/Layout.vue
+++ src/components/common/Layout.vue
@@ -3,7 +3,7 @@ import { ref } from 'vue';
 import Header from './Header.vue'
 import Footer from './Footer.vue'
 import CommonModal from "./ModalContainer.vue";
-
+import CommonHeader from "./CommonHeader.vue";
 const isModalVisible = ref(false);
 
 function openModal() {
@@ -15,6 +15,7 @@ function openModal() {
   <div id="layout" class="container mx-auto">
     <Header />
     <slot />
+    <CommonHeader />
     <button @click="openModal">Open Modal</button> <!-- 모달을 열기 위한 버튼 -->
     <CommonModal :isVisible="isModalVisible" @close="isModalVisible = false">
       <p>Modal Content Here</p>
