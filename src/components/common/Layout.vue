3867fd586c5aba45013118f9cdfee2a96865117d
bizbaeja <bjgbjg112@gmail.com>
2024-05-26 13:34:23 +0900

    feat: ModalContainer 제작

--- src/components/common/Layout.vue
+++ src/components/common/Layout.vue
@@ -1,16 +1,27 @@
 <script setup>
-  import Header from './Header.vue'
-  import Footer from './Footer.vue'
+import { ref } from 'vue';
+import Header from './Header.vue'
+import Footer from './Footer.vue'
+import CommonModal from "./ModalContainer.vue";
+
+const isModalVisible = ref(false);
+
+function openModal() {
+  isModalVisible.value = true;
+}
 </script>
 
 <template>
   <div id="layout" class="container mx-auto">
     <Header />
     <slot />
+    <button @click="openModal">Open Modal</button> <!-- 모달을 열기 위한 버튼 -->
+    <CommonModal :isVisible="isModalVisible" @close="isModalVisible = false">
+      <p>Modal Content Here</p>
+    </CommonModal>
     <Footer />
   </div>
 </template>
-
 
 <style lang="scss" scoped>
 
