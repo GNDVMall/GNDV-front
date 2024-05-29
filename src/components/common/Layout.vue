6337f6c2a65b5639a20c915ccd18c955a83f3969
bizbaeja <bjgbjg112@gmail.com>
2024-05-29 16:05:56 +0900

    feat: wish-view 화면 구성 임시

--- src/components/common/Layout.vue
+++ src/components/common/Layout.vue
@@ -15,11 +15,10 @@ function openModal() {
   <div id="layout" class="container mx-auto">
     <Header />
     <slot />
-    <CommonHeader />
     <button @click="openModal">Open Modal</button> <!-- 모달을 열기 위한 버튼 -->
-    <CommonModal :isVisible="isModalVisible" @close="isModalVisible = false">
+    <!-- <CommonModal :isVisible="isModalVisible" @close="isModalVisible = false">
       <p>Modal Content Here</p>
-    </CommonModal>
+    </CommonModal> -->
     <Footer />
   </div>
 </template>
