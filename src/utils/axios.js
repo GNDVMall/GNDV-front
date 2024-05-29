a7534c9a1c0bcd626b8dcea6417ead91224c51e5
12Ahn22 <12ayo22@gmail.com>
2024-05-29 20:06:26 +0900

    feat: 공용 axios 통신 메서드

---
+++ src/utils/axios.js
@@ -0,0 +1,49 @@
+import axios from 'axios'
+import { ref, isRef, unref, watchEffect } from 'vue'
+
+// Axios Config
+const instance = axios.create({
+  baseURL: 'http://localhost:8080/api/', // 후에 URL 변경
+  timeout: 1000,
+});
+
+
+// useAxios
+export function useAxios(method, url, input, headers) {
+  const data = ref(null)
+  const error = ref(null)
+
+  async function doAxios() {
+    data.value = null
+    error.value = null
+
+    // watchEffect()에 의해 종속성으로 추적되도록
+    // URL 값을 동기식으로 resolve합니다.
+    const urlValue = unref(url)
+
+    try {
+      // axios method mapping
+      const methods = {
+        get: () => instance.get(urlValue, headers),
+        post: () => instance.post(urlValue, input, headers),
+        put: () => instance.put(urlValue, input, headers),
+        delete: () => instance.delete(urlValue, headers),
+      }
+
+      const res = await (methods[method.toLowerCase()] || (() => { throw new Error(`Unsupported method: ${method}`) }))();
+      data.value = res.data;
+    } catch (e) {
+      error.value = e
+    }
+  }
+
+  if (isRef(url)) {
+    // 설정하기: 입력 URL이 ref인 경우 반응적 다시 가져오기
+    watchEffect(doAxios)
+  } else {
+    // 그렇지 않으면 한 번만 가져오기
+    doAxios()
+  }
+
+  return { data, error, retry: doAxios }
+}
