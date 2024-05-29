698f27710d3affb3e1d92825db4a665fbc61f5bf
bizbaeja <bjgbjg112@gmail.com>
2024-05-29 14:16:20 +0900

    fix: props 가능하게 변경

--- src/components/common/CommonHeader.vue
+++ src/components/common/CommonHeader.vue
@@ -1,22 +1,29 @@
 <template>
-    <header class=" py-4">
-      <div class="container mx-auto">
-        <div class="flex items-center">
-          <h1 class="text-xl font-bold">로그인 정보</h1>
-        </div>
-        <div class="relative mt-2">
-          <div class="absolute inset-x-0 top-[-8px] h-1 "></div>
-          <div class="h-1 bg-black mt-2"></div>
-        </div>
+  <header class="py-4">
+    <div class="container mx-auto">
+      <div class="flex items-center">
+        <h1 class="text-xl font-bold">{{ title }}</h1>
       </div>
-    </header>
-  </template>
-  
-  <script setup>
-  // Here you can add any necessary script, if needed.
-  </script>
-  
-  <style scoped>
-  /* Custom styles can be added here if needed. */
-  </style>
-  
+      <div class="relative mt-2">
+        <div class="absolute inset-x-0 top-[-8px] h-1 "></div>
+        <div class="h-1 bg-black mt-2"></div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+    default: '로그인 정보'
+  }
+});
+</script>
+
+<style scoped>
+/* Custom styles can be added here if needed. */
+</style>
