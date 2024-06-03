d59384eba64021ae980a048a575c008ff1d0d266
12Ahn22 <12ayo22@gmail.com>
2024-06-03 18:59:10 +0900

    feat : 상품 세부 정보 타이틀 퍼블리싱

---
+++ src/components/items/ItemSubInfo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :class="['flex flex-col items-center border-slate-300', borderClass]">
+    <p class="opacity-60 text-xs">{{ text }}</p>
+    <p class="text-sm">{{ subText }}</p>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'none'
+  },
+  text: String,
+  subText: String
+});
+
+const border = {
+  right: 'border-r',
+  none: ''
+}
+
+const borderClass = computed(() => border[props.type]);
+
+</script>
+
+<style scoped></style>
