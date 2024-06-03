40a0f1162a8d5767a77c92d3e5a52934ec436764
12Ahn22 <12ayo22@gmail.com>
2024-06-03 18:21:11 +0900

    feat: Item Title 퍼블리싱

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import ChatView from '../views/ChatView.vue'
 import LoginView from '../views/LoginView.vue'
 import NoticeView from '../views/NoticeView.vue'
 import WishView from '../views/WishView.vue'
+import ItemView from '../views/ItemView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -42,6 +43,11 @@ const router = createRouter({
       path: '/notice',
       name: 'notice',
       component: NoticeView
+    },
+    {
+      path:'/items/:id',
+      name:'item',
+      component: ItemView
     }
   ]
 })
