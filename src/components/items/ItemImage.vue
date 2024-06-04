8b419c14359eebff519548caa56f1eb7b3711647
12Ahn22 <12ayo22@gmail.com>
2024-06-04 09:55:06 +0900

    feat: Item 이미지 컴포넌트 분리

---
+++ src/components/items/ItemImage.vue
@@ -0,0 +1,24 @@
+<template>
+    <div
+      class="w-full md:max-w-lg aspect-square object-cover  place-content-center flex bg-gray-200 border border-gray-200">
+      <img class="w-full object-contain" :src="url" :alt="alt">
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    url: {
+        type:String,
+        required: true
+    },
+    alt:{
+        type:String,
+        required: true
+    }
+})
+
+</script>
+
+<style scoped>
+
+</style>
