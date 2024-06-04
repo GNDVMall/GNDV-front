80399499b8a27e3887d26f189cf8a0cc8f1de2f3
12Ahn22 <12ayo22@gmail.com>
2024-06-04 09:56:28 +0900

    feat: ItemView 페이지 중간 완성 커밋 - 상단 Item Card 퍼블리싱 완성

---
+++ src/views/ItemView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import ItemCard from '@/components/items/ItemCard.vue';
+
+</script>
+
+<template>
+    <main>
+        <h2>{{ $route.params.id }}</h2>
+        <!-- 상단 Item 정보 -->
+        <ItemCard />
+
+        <!-- 중고 판매 목록 -->
+    </main>
+</template>
