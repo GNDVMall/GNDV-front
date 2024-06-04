5255acb9d8b56f5a4b402ec669410cdcdd2c2554
12Ahn22 <12ayo22@gmail.com>
2024-06-04 19:14:41 +0900

    feat: Suspense 적용 - 로딩 스피너 전체 처리 - 에러 처리

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,40 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 import LayoutVue from './components/common/Layout.vue'
+
+import { onErrorCaptured, ref } from 'vue'
+const hasError = ref(false)
+const errorCode = ref(null)
+
+onErrorCaptured((e) => {
+  console.error(e)
+  hasError.value = true
+  errorCode.value = e.response.data || 403;
+  return false
+})
 
 </script>
 
 <template>
-  <LayoutVue>
-    <RouterView />
-  </LayoutVue>
+  <RouterView v-slot="{ Component }">
+    <template v-if="Component">
+      <Transition mode="out-in">
+        <KeepAlive>
+            <LayoutVue>
+            <div v-if="hasError">에러 발생 {{errorCode }}</div>
+            <Suspense v-else>
+              <!-- 로딩 상태 -->
+              <template #fallback>
+                로딩중...
+              </template>
+              <!-- 메인 컨텐츠 -->
+              <component :is="Component"></component>
+            </Suspense>
+          </LayoutVue>
+          </KeepAlive>
+        </Transition>
+      </template>
+    </RouterView>
 </template>
 
 <style scoped>
