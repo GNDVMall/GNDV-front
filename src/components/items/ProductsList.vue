5ff88413fe6717c1223977122dcf1b1d3c8ce353
12Ahn22 <12ayo22@gmail.com>
2024-06-04 12:54:54 +0900

    feat: Product Card도 공통 컴포넌트로 변경

--- src/components/items/ProductsList.vue
+++ src/components/items/ProductsList.vue
@@ -74,9 +74,8 @@
 
 <script setup>
 import Title from '@/components/common/Title/Title.vue'
-import ItemCard from '@/components/common/ItemCard/ItemCard.vue'
 import Button from '@/components/common/buttom/Buttom.vue';
-import ProductCard from '@/components/product/ProductCard.vue'
+import ProductCard from '@/components/common/ProductCard/ProductCard.vue'
 
 </script>
 
