0252b0b6567ac8fe2713e5b63783ca9ae7aaf208
CopyPaste <139524617+imhungry2@users.noreply.github.com>
2024-06-13 18:02:43 +0900

    Feat/search imhungry2 (#33)
    * feat(Search): 검색 작업
    
    * fix: 커밋
    
    * feat(Search): 검색 이미지 작업

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -3,15 +3,15 @@
     <!-- sub nav -->
     <div class="container mx-auto flex justify-end items-center">
       <nav class="flex space-x-4">
-          <RouterLink to="/login"><span href="#" class="text-gray-600 text-xs">로그인</span></RouterLink>
-          <RouterLink to="/notice"><span href="#" class="text-gray-600 text-xs">공지사항</span></RouterLink>
-          <RouterLink to="/wish"><span href="#" class="text-gray-600 text-xs">관심</span></RouterLink>
-          <RouterLink to="/"><span href="#" class="text-gray-600 text-xs">알림</span></RouterLink>
+        <RouterLink to="/login"><span href="#" class="text-gray-600 text-xs">로그인</span></RouterLink>
+        <RouterLink to="/notice"><span href="#" class="text-gray-600 text-xs">공지사항</span></RouterLink>
+        <RouterLink to="/wish"><span href="#" class="text-gray-600 text-xs">관심</span></RouterLink>
+        <RouterLink to="/"><span href="#" class="text-gray-600 text-xs">알림</span></RouterLink>
       </nav>
     </div>
     <div class="container mx-auto flex justify-between items-center">
       <!-- logo -->
-      <div class="logo"><RouterLink to="/"><img src="/public/logo/gndv-logo2.png"/></RouterLink></div>
+      <div class="logo"><RouterLink to="/"><img src="/public/logo/gndv-logo2.png" /></RouterLink></div>
 
       <!-- main nav -->
       <nav class="flex space-x-8 text-lg">
@@ -20,22 +20,36 @@
         <RouterLink to="/my"><span href="#" class="text-black">MY GNDB</span></RouterLink>
 
         <!-- 클릭 시, 검색 전체 모달 화면 -->
-        <div class="cursor-pointer"><i class="fa-solid fa-magnifying-glass"></i></div>
+        <div class="cursor-pointer" @click="openModal"><i class="fa-solid fa-magnifying-glass"></i></div>
       </nav>
     </div>
+
+    <!-- Search Modal -->
+    <SearchModal :isOpen="isModalOpen" @close="closeModal" />
   </header>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import SearchModal from '../search/SearchModal.vue'
 
+const isModalOpen = ref(false)
+
+const openModal = () => {
+  isModalOpen.value = true
+}
+
+const closeModal = () => {
+  isModalOpen.value = false
+}
 </script>
 
 <style scoped>
-  .logo{
-    width: 5rem;
-  }
-  .logo img{
-    width: 100%;
-    object-fit: contain;
-  }
+.logo {
+  width: 5rem;
+}
+.logo img {
+  width: 100%;
+  object-fit: contain;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -21,6 +21,7 @@ import ProductEditView from '@/views/ProductEditView.vue'
 import OrderHistory from '@/components/order/OrderHistory.vue'
 import SalesHistory from '@/components/order/SalesHistory.vue'
 import ModalContainer from '@/components/modal/ModalContainer.vue'
+import SearchResultsView from '@/views/SearchResultsView.vue' // 검색 결과 페이지 추가
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -41,8 +42,8 @@ const router = createRouter({
           path: 'purchase',
           name: 'purchase',
           component: OrderHistory
-        }
-        ,{
+        },
+        {
           path:'sales',
           name:'sales',
           component: SalesHistory
@@ -118,12 +119,16 @@ const router = createRouter({
       path: '/payment-test',
       name: 'PaymentTest',
       component: PaymentTest
-    }
-    ,
-    { 
+    },
+    {
       path: '/review',
       name: 'Review',
       component: ModalContainer
+    },
+    {
+      path: '/search-results',
+      name: 'SearchResults',
+      component: SearchResultsView
     }
   ]
 })
