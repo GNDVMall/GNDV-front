7be6713dc7affbcc8f05593337050e3eaf942a8e
12Ahn22 <44540726+12Ahn22@users.noreply.github.com>
2024-06-13 19:55:29 +0900

    Feat: 채팅 이미지 기능 완성 (#34)
    * style: 사용안하는 코드 삭제
    
    * refactor: 채팅 기능을 textarea로 변경
    - toastUI 불친절, vue3 버전 없음, 관리가 안되는 것 같음.
    
    * style: CSS 깨지던 현상 수정
    
    * fix: 잘못 덮혀진 CardItemCard 복구
    
    * fix: 채팅 시, 영어가 되지않는 현상 수정
    
    * feat: 채팅 메시지가 이미지인 경우 이미지가 출력되도록 기능 추가
    
    * feat: 이미지 클릭 시, 전체 화면 모달

--- src/components/chat/ChatMessage.vue
+++ src/components/chat/ChatMessage.vue
@@ -1,28 +1,35 @@
 <template>
   <li v-if="userType !== 'SYSTEM'" :class="['flex', messageClass]">
-    <img v-if="type !== 'SENT'" src="https://cdn.mos.cms.futurecdn.net/7auVjCELrhFKTPfudXRTgc.jpg" alt="Avatar" class="w-10 h-10 rounded-full object-cover mr-3">
+    <img v-if="type !== 'SENT'" src="https://cdn.mos.cms.futurecdn.net/7auVjCELrhFKTPfudXRTgc.jpg" alt="프로필" class="w-10 h-10 rounded-full object-cover mr-3">
     <div class="gap-2" :class="[type === 'SENT' ? 'order-1' : '', 'flex items-start']">
-      <span v-if="props.type === 'SENT'" class="text-sm text-gray-500 mt-2">
-        {{ formatTime(props.date) }}
+      <span v-if="type === 'SENT'" class="text-sm text-gray-500 mt-2">
+        {{ formatTime(date) }}
       </span>
       <div :class="['p-3 rounded-lg max-w-60', bubbleClass]">
-        <p class="text-gray-800">
-          {{ props.content }}
+        <div v-if="contentType === 'IMAGE'">
+          <img :src="content" alt="이미지"  @click="toggleImage"/>
+        </div>
+        <p v-else class="text-gray-800">
+          {{ content }}
         </p>
       </div>
-      <span v-if="props.type !== 'SENT'" class="text-sm text-gray-500 mt-2">
-        {{ formatTime(props.date) }}
+      <span v-if="type !== 'SENT'" class="text-sm text-gray-500 mt-2">
+        {{ formatTime(date) }}
       </span>
     </div>
   </li>
   <li v-else>
     <div class="text-center p-2">상대방이 채팅방을 떠났습니다.</div>
   </li>
+  <div v-if="toggle" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-80 z-50" @click="toggleImage">
+    악
+    <img :src="content" alt="이미지" class="max-w-90 max-h-90">
+  </div>
 </template>
 
 <script setup>
 import { formatTime } from '@/utils/dateUtils';
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { defineProps } from 'vue';
 
 const props = defineProps({
@@ -31,13 +38,20 @@ const props = defineProps({
     required: true,
     validator: value => ['SENT', 'RECEIVE'].includes(value)
   },
-  content:String,
+  content: String,
   date: String,
-  userType: String
+  userType: String,
+  contentType: String
 });
 
 const messageClass = computed(() => props.type === 'SENT' ? 'justify-end' : 'justify-start');
 const bubbleClass = computed(() => props.type === 'SENT' ? 'bg-blue-100 rounded-tr-none' : 'bg-gray-100 rounded-tl-none');
+const toggle = ref(false)
+
+const toggleImage = ()=>{
+  toggle.value = !toggle.value
+}
+
 </script>
 
 <style scoped>
