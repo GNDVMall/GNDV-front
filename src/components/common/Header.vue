a565b894583bcf7a8fb9946aefb2d344055078be
12Ahn22 <12ayo22@gmail.com>
2024-06-14 11:00:28 +0900

    style: 헤더 container css 변경

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,15 +1,15 @@
 <template>
   <header class="w-full bg-white py-4">
     <!-- sub nav -->
-    <div class="container mx-auto flex justify-end items-center">
+    <div class=" w-full flex justify-end items-center">
       <nav class="flex space-x-4">
         <RouterLink to="/login"><span href="#" class="text-gray-600 text-xs">로그인</span></RouterLink>
-        <RouterLink to="/notice"><span href="#" class="text-gray-600 text-xs">공지사항</span></RouterLink>
+        <!-- <RouterLink to="/notice"><span href="#" class="text-gray-600 text-xs">공지사항</span></RouterLink> -->
         <RouterLink to="/wish"><span href="#" class="text-gray-600 text-xs">관심</span></RouterLink>
-        <RouterLink to="/"><span href="#" class="text-gray-600 text-xs">알림</span></RouterLink>
+        <!-- <RouterLink to="/"><span href="#" class="text-gray-600 text-xs">알림</span></RouterLink> -->
       </nav>
     </div>
-    <div class="container mx-auto flex justify-between items-center">
+    <div class="w-full flex justify-between items-center">
       <!-- logo -->
       <div class="logo"><RouterLink to="/"><img src="/public/logo/gndv-logo2.png" /></RouterLink></div>
 
@@ -17,7 +17,7 @@
       <nav class="flex space-x-8 text-lg">
         <RouterLink to="/"><span href="#" class="text-black font-bold">HOME</span></RouterLink>
         <RouterLink to="/chat"><span href="#" class="text-black">CHAT</span></RouterLink>
-        <RouterLink to="/my"><span href="#" class="text-black">MY GNDB</span></RouterLink>
+        <RouterLink to="/my"><span href="#" class="text-black">MY GNDV</span></RouterLink>
 
         <!-- 클릭 시, 검색 전체 모달 화면 -->
         <div class="cursor-pointer" @click="openModal"><i class="fa-solid fa-magnifying-glass"></i></div>
