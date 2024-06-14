bc13c26ace5b83ac1884bdca365318a72380477b
bizbaeja <75407823+bizbaeja@users.noreply.github.com>
2024-06-14 10:20:32 +0900

    feat: 리뷰기능 (#35)
    * feat: ModalContainer 제작
    
    * feat: CommonHeader 추가
    
    * fix: props 가능하게 변경
    
    * feat: wish-view 화면 구성 임시
    
    * fix: 수정사항 반영
    
    * refactor: dateUtil 함수 분리
    
    * feat: wishlist 구현
    
    * refactor: 로그인한 유저의 목록만 보이게
    
    * feat:판매내역 기능 추가
    
    * feat: 마이페이지 사이드바
    
    * fix: 구매내역 못불러오는 문제 해결
    
    * feat : 별점
    
    * feat: 중복리뷰방지
    
    * fix: 고침
    
    * feat: 구매 및 판매 내역
    
    * 중간저장 : 판매내역 로드까지
    
    * feat : 리뷰작성 성공
    
    * feat : 리뷰 중복작성 방지
    
    * feat: 판매내역 리뷰 모달
    
    * 리뷰 중간 저장
    
    * fix : css 수정
    
    * feat: 구매내역 리뷰
    
    * feat: 구매내역 중복작성 방지
    
    * fix
    
    * fix2

--- src/components/order/SalesHistory.vue
+++ src/components/order/SalesHistory.vue
@@ -1,77 +1,93 @@
 <template>
   <div>
-    <CommonHeader title="판매내역"/>
+    <CommonHeader title="판매내역" />
     <div class="sale-list bg-white shadow-md rounded-md p-4">
-      <div v-for="sale in filteredSales" :key="sale.order_uid" class="mb-3 p-4 border-b border-gray-200">
+      <div
+        v-for="sale in filteredSales"
+        :key="sale.order_uid"
+        class="mb-3 p-4 border-b border-gray-200"
+      >
         <div class="flex justify-between items-center">
           <div>
             <h5 class="font-bold text-lg">{{ sale.item_name }}</h5>
             <p class="text-sm text-gray-600">가격: {{ sale.price }}원</p>
-            <p class="text-sm text-gray-600">구매자: {{ sale.buyer_name }} ({{ sale.buyer_email }})</p>
-            <p class="text-sm text-gray-600">구매자 전화번호: {{ sale.buyer_tel }}</p>
-            <p class="text-sm text-gray-600">우편번호: {{ sale.buyer_postcode }}</p>
+            <p class="text-sm text-gray-600">
+              구매자: {{ sale.buyer_name }} ({{ sale.buyer_email }})
+            </p>
+            <p class="text-sm text-gray-600">
+              구매자 전화번호: {{ sale.buyer_tel }}
+            </p>
+            <p class="text-sm text-gray-600">
+              우편번호: {{ sale.buyer_postcode }}
+            </p>
             <p class="text-sm text-gray-600">주문번호: {{ sale.order_uid }}</p>
-            <p class="text-sm text-gray-600">결제 상태: {{ sale.payment_status || 'N/A' }}</p>
-            <p class="text-sm text-gray-600">결제 가격: {{ sale.paymentPrice || 'N/A' }}원</p>
+            <p class="text-sm text-gray-600">
+              결제 상태: {{ sale.payment_status || "N/A" }}
+            </p>
+            <p class="text-sm text-gray-600">
+              결제 가격: {{ sale.paymentPrice || "N/A" }}원
+            </p>
           </div>
-          <button 
-            :class="{'btn btn-primary': true, 'btn-secondary': sale.review_id}" 
-            :disabled="sale.review_id" 
-            @click="openReviewModal(sale)">
+          <button
+            :class="{
+              'btn btn-primary': true,
+              'btn-secondary': sale.review_id,
+            }"
+            :disabled="sale.review_id"
+            @click="openReviewModal(sale)"
+          >
             리뷰 작성
           </button>
         </div>
       </div>
     </div>
 
-    <SellerReviewModal v-if="showModal" :isVisible="showModal" :productId="selectedSale.product_id" @close="closeReviewModal" />
+    <SellerReviewModal
+      v-if="showModal"
+      :isVisible="showModal"
+      :productId="selectedSale.product_id"
+      @close="closeReviewModal"
+    />
   </div>
 </template>
 
 <script setup>
-import { ref, computed, onMounted } from 'vue';
-import instance from '@/utils/axios';
-import CommonHeader from '@/components/common/CommonHeader.vue';
-import SellerReviewModal from '@/components/modal/SellerReviewModal.vue';
-
+import { ref, computed, onMounted } from "vue";
+import instance from "@/utils/axios";
+import CommonHeader from "@/components/common/CommonHeader.vue";
+import SellerReviewModal from "@/components/modal/SellerReviewModal.vue";
 const sales = ref([]);
 const showModal = ref(false);
 const selectedSale = ref(null);
-
 const openReviewModal = (sale) => {
   selectedSale.value = sale;
   showModal.value = true;
 };
-
 const closeReviewModal = () => {
   showModal.value = false;
   selectedSale.value = null;
 };
-
 const filteredSales = computed(() => {
-  return sales.value.filter(sale => sale.product_id !== null);
+  return sales.value.filter((sale) => sale.product_id !== null);
 });
-
 onMounted(async () => {
   try {
-    const response = await instance.get('/salesList');
+    const response = await instance.get("/salesList");
     if (response.data && response.data.code === 200) {
       sales.value = response.data.data;
       console.log("sales.value:", sales.value);
     } else {
-      console.error('Error fetching sales:', response.data.message);
+      console.error("Error fetching sales:", response.data.message);
     }
   } catch (error) {
-    console.error('Error fetching sales:', error);
+    console.error("Error fetching sales:", error);
   }
 });
 </script>
-
 <style scoped>
 .sale-list {
   padding: 2rem;
 }
-
 .btn-secondary {
   background-color: #6c757d;
   cursor: not-allowed;
--- src/router/index.js
+++ src/router/index.js
@@ -21,7 +21,9 @@ import ProductEditView from '@/views/ProductEditView.vue'
 import OrderHistory from '@/components/order/OrderHistory.vue'
 import SalesHistory from '@/components/order/SalesHistory.vue'
 import ModalContainer from '@/components/modal/ModalContainer.vue'
+
 import SearchResultsView from '@/views/SearchResultsView.vue' // 검색 결과 페이지 추가
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
