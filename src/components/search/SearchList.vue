794eb238841838092957dce5a6ad62dd48890b78
12Ahn22 <12ayo22@gmail.com>
2024-06-15 13:44:20 +0900

    feat: 옵션 필터 적용

--- src/components/search/SearchList.vue
+++ src/components/search/SearchList.vue
@@ -2,7 +2,21 @@
       <div v-if="items" class="w-full">
         <div class="flex justify-between mb-4">
           <p class="text-sm">상품 {{ items.list.length }}</p>
-          <button class="text-sm">정렬</button>
+          <div class="w-full max-w-32 text-sm">
+            <select
+              id="sort"
+              v-model="selectedOption"
+              class="outline-none"
+            >
+              <option v-for="option in sortOptions" :key="option.key" :value="option.key">
+                {{ option.name }}
+              </option>
+            </select>
+            <button @click="handleSortOrderBy" class="ml-2 w-4 h-4">
+              <i v-if="orderBy === 'asc'" class="fa-solid fa-sort-up"></i>
+              <i v-else="orderBy === 'desc'" class="fa-solid fa-sort-down"></i>
+            </button>
+          </div>
         </div>
         <div
           v-if="items.list.length > 0"
@@ -28,13 +42,19 @@
 </template>
 
 <script setup>
-import { instance } from '@/utils/axios';
-import { onMounted, ref, watch } from 'vue';
-import { useRoute } from 'vue-router';
-import ItemCard from '../common/ItemCard/ItemCard.vue';
+import { instance } from '@/utils/axios'
+import { onMounted, ref, watch } from 'vue'
+import { useRoute } from 'vue-router'
+import ItemCard from '../common/ItemCard/ItemCard.vue'
+import router from '@/router';
 
-const route = useRoute();
-const items = ref(null);
+const route = useRoute()
+const items = ref(null)
+
+const sortOptions = [{key:'item_name', name:'이름'}, {key:'age_range', name:'연령'},{key:'recent_price', name:'최근 거래가'},{key:'regular_price',name:'정가'}]
+const selectedOption = ref('item_name')
+const orderBy = ref('asc')
+
 
 // 검색 결과를 호출
 const fetchSearchResults = async () => {
@@ -48,6 +68,11 @@ const fetchSearchResults = async () => {
   }
 };
 
+const handleSortOrderBy = ()=>{
+  if(orderBy.value === 'desc') orderBy.value = 'asc'
+  else orderBy.value = 'desc'
+}
+
 onMounted(() => {
   fetchSearchResults();
 });
@@ -56,8 +81,25 @@ watch(() => route.query, ()=>{
   fetchSearchResults()
 });
 
+watch(() => selectedOption.value, ()=>{
+  if(selectedOption.value){
+    const query = new URLSearchParams(route.query)
+    query.delete('sortBy')
+    query.append('sortBy', selectedOption.value)
+    router.push(`?${query.toString()}`)
+  }
+});
+
+watch(() => orderBy.value, ()=>{
+  if(orderBy.value){
+    const query = new URLSearchParams(route.query)
+    query.delete('sortOrder')
+    query.append('sortOrder', orderBy.value)
+    router.push(`?${query.toString()}`)
+  }
+});
+
 </script>
 
 <style scoped>
-
 </style>
