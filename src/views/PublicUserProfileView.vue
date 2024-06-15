a82c3296daff20caa725b9155bdbc93a46ccd839
12Ahn22 <12ayo22@gmail.com>
2024-06-15 15:08:44 +0900

    feat: 유저 상단 카드 퍼블리싱

---
+++ src/views/PublicUserProfileView.vue
@@ -0,0 +1,11 @@
+<template>
+  <UserProfileCard />
+</template>
+
+<script setup>
+import UserProfileCard from '@/components/publicProfile/UserProfileCard.vue'
+</script>
+
+<style scoped>
+
+</style>
