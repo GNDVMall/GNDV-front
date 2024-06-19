fee113867bd37410657ed75408728a3d4e3a886c
12Ahn22 <12ayo22@gmail.com>
2024-06-19 14:42:57 +0900

    style: 사용 안하는 코드 삭제

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <template>
   <main class="flex flex-col items-center justify-center h-full">
-    <LoadingSpinner :visible="isLoading" />
     <CarouselComponent class="mt-11" />
     <RecentProductList class="mt-11" />
     <ThemeList class="mt-11" />
@@ -8,23 +7,10 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
-import LoadingSpinner from "@/components/common/Loader/LoadingSpinner.vue";
 import CarouselComponent from "@/components/common/carousel/CarouselComponent.vue";
 import RecentProductList from "@/components/common/ProductCard/RecentProductList.vue";
 import ThemeList from "@/components/theme/ThemeList.vue";
-const isLoading = ref(true);
-
-onMounted(async () => {
-  try {
-  } catch (error) {
-    console.error("Error during data fetch:", error);
-  } finally {
-    isLoading.value = false;
-  }
-});
 </script>
 
 <style>
-/* Add any necessary global styles here */
 </style>
