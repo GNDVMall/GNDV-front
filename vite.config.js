0e6e123f736fed06a643da0a9b545d481eedbfa3
bizbaeja <bjgbjg112@gmail.com>
2024-06-21 09:17:50 +0900

    cors

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,24 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import VueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import VueDevTools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
+  plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://43.200.252.4:8080", // 백엔드 서버 주소
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""), // '/api'로 시작하는 경로를 백엔드 서버로 프록시
+      },
+    },
+  },
+});
