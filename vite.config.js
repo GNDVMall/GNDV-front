69d81dbceb3cc57dd833cf596cb51b1646f50fdc
bizbaeja <bjgbjg112@gmail.com>
2024-06-23 22:30:32 +0900

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,13 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import VueDevTools from "vite-plugin-vue-devtools";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
-import commonjs from "vite-plugin-commonjs";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VueDevTools(), nodePolyfills(), commonjs()],
+  plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-  },
-  build: {
-    commonjsOptions: { transformMixedEsModules: true }, // Change
   },
 });
